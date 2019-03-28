17b1ea92cf645ae070ea5fa890de45d2e83cd700
by_6 <bysiek1998@gmail.com>
2019-03-28 11:22:49 +0100

    initialize repo

---
+++ .history/main_20190326210435.js
@@ -0,0 +1,10 @@
+let speed = 10;
+let logoElement = document.getElementById("logo");
+let backgroundElement = document.getElementById("background");
+let animation = function () {
+    let currentY = logoElement.style.top
+    let currentX = logoElement.style.left
+    let backgroundWidth = backgroundElement.style.width
+
+
+}
---
+++ .history/main_20190326212248.js
@@ -0,0 +1,29 @@
+let speed = 100;
+let velocityX = 1;
+let velocityY = 1;
+let logoElement = document.getElementById("logo");
+let backgroundElement = document.getElementById("background");
+console.log(backgroundElement.style);
+logoElement.style.top = "10px";
+logoElement.style.left = "10px";
+backgroundElement.style.width = "1600px"
+let animation = function () {
+    let currentY = parseInt(logoElement.style.top);
+    let currentX = parseInt(logoElement.style.left);
+    let backgroundWidth = backgroundElement.style.width;
+    let backgroundHeight = parseInt(backgroundElement.style.y);
+    console.log(currentY + "|" + backgroundWidth)
+    if (currentY >= backgroundHeight || currentY <= 0) {
+        valocityY = -velocityY;
+        console.log("xDD");
+    }
+    if (currentX >= backgroundWidth || currentX <= 0) {
+        velocityX = -velocityX;
+        console.log("xD");
+    }
+    logoElement.style.top = (currentY + speed) * velocityY + "px";
+    logoElement.style.left = (currentX + speed) * velocityX + "px";
+}
+animation();
+
+var timer = window.setInterval(animation, 1000);
